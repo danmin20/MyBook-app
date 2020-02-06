73e663046930884e7a4e7b5f7e16461ae230ac02
Lee Jeong Min <danmin20@khu.ac.kr>
2020-02-06 15:59:56 +0900

    home screen style

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { SEARCH } from "../Queries";
+import { SEARCH } from "../gql/queries";
 import { TouchableOpacity } from "react-native";
 import styles from "../styles";
 import { useQuery } from "react-apollo-hooks";
